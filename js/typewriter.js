fb6d363b158b9d639562328c865fa28350aa8e26
Leo Febba <leonardofebba@hotmail.com>
2023-11-20 15:54:24 -0300

    Added content to Quem Somos page and fixed minor bugs

---
+++ js/typewriter.js
@@ -0,0 +1,34 @@
+const texts = [
+    "Moldando o futuro através de tecnologia",
+    "Conectamos escolhas a oportunidades ilimitadas",
+    "Inovação e excelência - nosso compromisso com o futuro"
+];
+
+let currentTextIndex = 0; 
+let currentIndex = 0;
+const speed = 50; 
+const repeatDelay = 2000; 
+
+function typeWriter() {
+    if (currentTextIndex < texts.length) {
+        const currentText = texts[currentTextIndex];
+        if (currentIndex < currentText.length) {
+            document.getElementById("content").innerHTML += currentText.charAt(currentIndex);
+            currentIndex++;
+            setTimeout(typeWriter, speed);
+        } else {
+            setTimeout(() => {
+                currentIndex = 0;
+                currentTextIndex++;
+                document.getElementById("content").innerHTML = "";
+                typeWriter();
+            }, repeatDelay);
+        }
+    } else {
+        currentTextIndex = 0;
+        typeWriter(); 
+    }
+}
+window.onload = function () {
+    typeWriter();
+};
